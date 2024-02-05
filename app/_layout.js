2aabb473ea89a874a0fc8a05f740b7c4b66f077f
Daniel Felipe Rosero Zemanate <dandan32099@gmail.com>
2024-02-04 23:18:10 -0500

    Adding PopularJobs component

--- app/_layout.js
+++ app/_layout.js
@@ -1,28 +1,27 @@
+import React, { useEffect, useState } from 'react';
 import { Stack } from 'expo-router';
-import { useCallback } from 'react';
 import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
 
-SplashScreen.preventAutoHideAsync();
-
-const Layout = () => { 
-    const [fontsLoaded] = useFonts({
+const Layout = () => {
+    const [isFontsLoaded] = useFonts({
         DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
         DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
         DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
     });
 
-    const onLayoutRootView = useCallback(async () => {
-        if (fontsLoaded) {
-            await SplashScreen.hideAsync();
-        }
-    }, [fontsLoaded]);
+    const [areFontsLoaded, setAreFontsLoaded] = useState(false);
 
-    if (!fontsLoaded) {
+    useEffect(() => {
+        if (isFontsLoaded) {
+            setAreFontsLoaded(true);
+        }
+    }, [isFontsLoaded]);
+
+    if (!areFontsLoaded) {
         return null;
     }
 
-    return <Stack onLayout={onLayoutRootView} />;
-}
+    return <Stack />;
+};
 
 export default Layout;
